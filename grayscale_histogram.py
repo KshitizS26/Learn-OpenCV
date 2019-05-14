60a9cc2550587784fffb28ffb4b7f216e10bac84
Kshitiz Sahay <kshitiz.sahay26@gmail.com>
2019-05-14 12:06:32 +0530

    Added grayscale_histogram.py

---
+++ grayscale_histogram.py
@@ -0,0 +1,7 @@
+from matplotlib import pyplot as plt
+import argparse
+import cv2
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required = True, help = "Path to the image")
+args = vars(ap.parse_args())
